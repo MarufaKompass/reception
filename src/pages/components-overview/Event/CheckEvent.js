8af3c3afaa26d94876095af040dcfbe9c6392f55
MarufaKompass <Marufa@hellokompass.com>
2023-11-19 12:51:50 +0600

    background Done

--- src/pages/components-overview/Event/CheckEvent.js
+++ src/pages/components-overview/Event/CheckEvent.js
@@ -1,8 +1,8 @@
 import MainCard from 'components/MainCard';
 import React, { useState } from 'react';
 import QrReader from 'react-qr-scanner';
-import { Box, OutlinedInput, Typography, Button } from '@mui/material';
-
+import { Box, OutlinedInput, Typography, Button, Paper } from '@mui/material';
+import Image from '../../../assets/images/img/reception_background.png';
 export default function CheckEvent() {
   const delay = 500;
 
@@ -23,52 +23,65 @@ export default function CheckEvent() {
   const handleError = (error) => {
     console.log(error);
   };
-
+  const styles = {
+    paperContainer: {
+      backgroundImage: `url(${Image})`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center center',
+      height: '80vh'
+    }
+  };
   return (
     <Box>
       <MainCard>
-        <Box>
-          <Typography
-            variant="h5"
-            color="#fff"
-            backgroundColor="#12A9B2"
-            sx={{ px: 3, py: 2, borderRadius: 1, display: 'flex', justifyContent: 'center' }}
-          >
-            Enter Event Code
-          </Typography>
-        </Box>
-        <Box sx={{ display: { sm: 'flex' }, justifyContent: 'center', alignItems: 'center', my: 2 }}>
-          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-            <QrReader delay={delay} style={previewStyle} onError={handleError} onScan={handleScan} />
+        <Paper style={styles.paperContainer}>
+          <Box>
+            <Typography
+              variant="h5"
+              color="#fff"
+              backgroundColor="#12A9B2"
+              sx={{ px: 3, py: 2, borderRadius: 1, display: 'flex', justifyContent: 'center' }}
+            >
+              Enter Event Code
+            </Typography>
           </Box>
-          <Box sx={{ mx: 2 }}>
-            <Typography variant="h6">Event Code *</Typography>
+          <Box display="flex" justifyContent="center" alignItems="center" height="80vh">
+            <Box backgroundColor="blue" width="60%">
+              <Box sx={{ display: { sm: 'flex' }, justifyContent: 'center', alignItems: 'center', my: 2 }} height="450px">
+                <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                  <QrReader delay={delay} style={previewStyle} onError={handleError} onScan={handleScan} />
+                </Box>
+                <Box sx={{ mx: 2 }}>
+                  <Typography variant="h6">Event Code *</Typography>
 
-            <OutlinedInput
-              id="outlined-adornment-weight"
-              aria-describedby="outlined-weight-helper-text"
-              sx={{ border: 1, borderColor: '#12A9B2', width: '100%', my: 1 }}
-              size="small"
-              value={resultQR}
-            />
-            <Box sx={{ display: 'flex', justifyContent: 'end' }}>
-              <Button
-                variant="outlined"
-                size="small"
-                sx={{ color: '#12A9B2', borderColor: '#12A9B2', '&:hover': { color: '#12A9B2', borderColor: '#12A9B2' }, mr: 1 }}
-              >
-                Submit
-              </Button>
-              <Button
-                variant="outlined"
-                size="small"
-                sx={{ color: '#FF0000', borderColor: '#FF0000', '&:hover': { color: '#FF0000', borderColor: '#FF0000' } }}
-              >
-                Cancel
-              </Button>
+                  <OutlinedInput
+                    id="outlined-adornment-weight"
+                    aria-describedby="outlined-weight-helper-text"
+                    sx={{ border: 1, borderColor: '#12A9B2', width: '100%', my: 1 }}
+                    size="small"
+                    value={resultQR}
+                  />
+                  <Box sx={{ display: 'flex', justifyContent: 'end' }}>
+                    <Button
+                      variant="outlined"
+                      size="small"
+                      sx={{ color: '#12A9B2', borderColor: '#12A9B2', '&:hover': { color: '#12A9B2', borderColor: '#12A9B2' }, mr: 1 }}
+                    >
+                      Submit
+                    </Button>
+                    <Button
+                      variant="outlined"
+                      size="small"
+                      sx={{ color: '#FF0000', borderColor: '#FF0000', '&:hover': { color: '#FF0000', borderColor: '#FF0000' } }}
+                    >
+                      Cancel
+                    </Button>
+                  </Box>
+                </Box>
+              </Box>
             </Box>
           </Box>
-        </Box>
+        </Paper>
       </MainCard>
     </Box>
   );
