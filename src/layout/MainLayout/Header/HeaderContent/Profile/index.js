6c5cd469f02d453784fb8b846fbed357025f858a
MarufaKompass <Marufa@hellokompass.com>
2023-11-20 11:10:19 +0600

    login page design

--- src/layout/MainLayout/Header/HeaderContent/Profile/index.js
+++ src/layout/MainLayout/Header/HeaderContent/Profile/index.js
@@ -1,13 +1,13 @@
-import { Avatar, Box, ButtonBase, Stack, Typography } from '@mui/material';
+import {  Box, ButtonBase, Stack, Typography } from '@mui/material';
 
-import avatar1 from 'assets/images/users/avatar-1.png';
+// import avatar1 from 'assets/images/users/avatar-1.png';
 
 const Profile = () => {
   return (
     <Box sx={{ flexShrink: 0, ml: 0.75 }}>
       <ButtonBase>
         <Stack direction="row" spacing={2} alignItems="center" sx={{ p: 0.5 }}>
-          <Avatar alt="profile user" src={avatar1} sx={{ width: 32, height: 32 }} />
+          {/* <Avatar alt="profile user" src={avatar1} sx={{ width: 32, height: 32 }} /> */}
           <Typography variant="subtitle1">Logout</Typography>
         </Stack>
       </ButtonBase>
--- src/pages/authentication/Login.js
+++ src/pages/authentication/Login.js
@@ -1,30 +1,50 @@
-import { Link } from 'react-router-dom';
-
 // material-ui
-import { Grid, Stack, Typography } from '@mui/material';
+import { Grid, Stack, Typography, Box } from '@mui/material';
 
 // project import
 import AuthLogin from './auth-forms/AuthLogin';
 import AuthWrapper from './AuthWrapper';
-
+import loginImage from '../../assets/images/img/login_img.png';
 // ================================|| LOGIN ||================================ //
 
 const Login = () => (
   <AuthWrapper>
-    <Grid container spacing={3}>
-      <Grid item xs={12}>
-        <Stack direction="row" justifyContent="space-between" alignItems="baseline" sx={{ mb: { xs: -0.5, sm: 0.5 } }}>
-          <Typography variant="h3">Login</Typography>
-          <Typography component={Link} to="/register" variant="body1" sx={{ textDecoration: 'none' }} color="primary">
-            Don&apos;t have an account?
-          </Typography>
+    <Grid container spacing={4}>
+      <Grid item xs={12} sm={12} md={12} lg={6} xl={6} sx={{ display: { xs: 'none', md: 'none', lg: 'block' } }}>
+        <Stack md={{ width: '100%' }}>
+          <Box sx={{ p: '25px' }}>
+            <img src={loginImage} alt="login" rel="Preload"></img>
+          </Box>
         </Stack>
       </Grid>
-      <Grid item xs={12}>
+
+      <Grid item xs={12} sm={12} md={12} lg={6} xl={6}>
+        <Stack textAlign="center" sx={{ mb: { xs: -0.5, sm: 0.5 } }}>
+          <Typography variant="h2">Sign in</Typography>
+          <Typography variant="p" sx={{ mt: 1 }}>
+            Login to stay connected.
+          </Typography>
+        </Stack>
         <AuthLogin />
       </Grid>
     </Grid>
   </AuthWrapper>
+
+  // <AuthWrapper>
+  //   <Grid container spacing={3}>
+  //     <Grid item xs={12}>
+  //       <Stack direction="row" justifyContent="space-between" alignItems="baseline" sx={{ mb: { xs: -0.5, sm: 0.5 } }}>
+  //         <Typography variant="h3">Login</Typography>
+  //         <Typography component={Link} to="/register" variant="body1" sx={{ textDecoration: 'none' }} color="primary">
+  //           Don&apos;t have an account?
+  //         </Typography>
+  //       </Stack>
+  //     </Grid>
+  //     <Grid item xs={12}>
+  //       <AuthLogin />
+  //     </Grid>
+  //   </Grid>
+  // </AuthWrapper>
 );
 
 export default Login;
