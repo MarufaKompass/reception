8ac470dbc4a7b9b0de9ece09e7be1d06b811c489
MarufaKompass <Marufa@hellokompass.com>
2025-02-13 15:51:57 +0600

    add table

--- src/AppContextReception.js
+++ src/AppContextReception.js
@@ -1,6 +1,5 @@
 import React, { createContext, useContext, useState } from 'react';
 
-
 const userStorage = JSON.parse(sessionStorage.getItem('user'));
 const comStorage = JSON.parse(sessionStorage.getItem('com'));
 const tokenStorage = JSON.parse(sessionStorage.getItem('token'));
@@ -14,12 +13,11 @@ export default function AppContextReception({ children }) {
   const [comId, setComId] = useState(comStorage);
   const [token, setToken] = useState(tokenStorage);
   const [lobbyData, setLobbyData] = useState([]);
+  const [restaurantLists, setRestaurantLists] = useState([]);
   const [hotelViewId, setHotelViewId] = useState(hotelIdStorage);
   const [apartmentId, setApartmentId] = useState('');
   const [rentalId, setRentalId] = useState('');
   const [buildingId, setBuildingId] = useState('');
-
- 
 
   return (
     <AppContext.Provider
@@ -41,7 +39,9 @@ export default function AppContextReception({ children }) {
         rentalId,
         setRentalId,
         buildingId,
-        setBuildingId
+        setBuildingId,
+        restaurantLists,
+        setRestaurantLists
       }}
     >
       {children}
--- src/pages/components-overview-restuarant/restaurantBookingList/RestaurantBookingList.js
+++ src/pages/components-overview-restuarant/restaurantBookingList/RestaurantBookingList.js
@@ -1,33 +1,51 @@
-import React, { useEffect } from 'react';
-import { Box, Typography, Button, OutlinedInput, InputAdornment, IconButton } from '@mui/material';
-import { DataGrid } from '@mui/x-data-grid';
-import { useTheme } from '@mui/material/styles';
-import { useMediaQuery } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Box, Typography, Grid } from '@mui/material';
+// import { DataGrid } from '@mui/x-data-grid';
+// import { useTheme } from '@mui/material/styles';
+// import { useMediaQuery } from '@mui/material';
 import MainCard from 'components/MainCard';
-import Search from 'components/svg/Search';
+// import Search from 'components/svg/Search';
 import { useAppContextReception } from 'AppContextReception';
 import axiosInstance from 'utils/axios.config';
 // import MeetingModal from 'components/modal/MeetingModal';
-import TableChip from '../../../components/chips/TableChip';
-import NoDataImage from 'components/Image/NoDataImage';
+// import TableChip from '../../../components/chips/TableChip';
+// import NoDataImage from 'components/Image/NoDataImage';
 import '../../../assets/styles.css';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 import { DatePicker } from 'antd';
-
+import dayjs from 'dayjs';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 export default function RestaurantBookingList() {
-  const { comId, lobbyData, setLobbyData, setHotelViewId } = useAppContextReception();
-  console.log(setHotelViewId);
+  const { restaurantLists, setRestaurantLists } = useAppContextReception();
+  const [handleDate, setHandleDate] = useState('');
 
-  const theme = useTheme();
-  const isSmallScreen = useMediaQuery(theme.breakpoints.down('md'));
-  // const [showMeetingModal, setShowMeetingModal] = useState(false);
+  const handleChange = (newDate) => {
+    setHandleDate(dayjs(newDate).format('YYYY-MM-DD'));
+  };
+
+  const [handleStatus, setHandleStatus] = useState('');
+  console.log('handleDate', handleDate);
+  console.log('handleStatus', handleStatus);
+
+  const handleChangeStatus = (event) => {
+    setHandleStatus(event.target.value);
+  };
 
   useEffect(() => {
     const fetchData = () => {
       axiosInstance
-        .get(`https://api.hellokompass.com/reception/bookinglist/${comId}`)
+        .get(`https://api.hellokompass.com/reception/restubookedlist?date=${handleDate}&status=${handleStatus}&offset=0&limit=30`)
         .then((res) => {
-          setLobbyData(res.data.data);
+          setRestaurantLists(res.data.data);
         })
         .catch((error) => {
           console.error(error);
@@ -37,202 +55,100 @@ export default function RestaurantBookingList() {
     fetchData();
     const interval = setInterval(fetchData, 10000);
     return () => clearInterval(interval);
-  }, [comId]);
+  }, [handleDate, handleStatus, setRestaurantLists]);
 
-  const navigate = useNavigate();
-  const handleBookingView = (booking_id) => {
-    sessionStorage.setItem('Book', JSON.stringify(booking_id));
-     setHotelViewId(booking_id);
-     navigate('/hotelView');
-  };
-
-  const adjustColumnWidths = () => {
-    const columns = [
-      { field: 'originalId', headerName: 'SL', width: 30, height: 50 },
-      {
-        field: 'Guest_Info',
-        headerName: 'Guest Info',
-        headerAlign: 'center',
-        align: 'left',
-        width: 220,
-        renderCell: (params) => (
-          <Box>
-            <Typography variant="body2">
-              {/* <Typography>{params.row.email}</Typography> */}
-              <Typography> {params.row.first_name + params.row.last_name}</Typography>
-              <Typography> {params.row.phone}</Typography>
-            </Typography>
-            <Typography variant="body2">{params.row.guest_phone}</Typography>
-          </Box>
-        )
-      },
-      {
-        field: 'Date_Form',
-        headerName: 'Date Form',
-        headerAlign: 'center',
-        align: 'center',
-        width: 150,
-        renderCell: (params) => (
-          <Box>
-            <Typography variant="body2"> {params.row.date_from}</Typography>
-          </Box>
-        )
-      },
-      {
-        field: 'Date_To',
-        headerName: 'Date To',
-        headerAlign: 'center',
-        align: 'center',
-        width: 150,
-        renderCell: (params) => (
-          <Box>
-            <Typography variant="body2"> {params.row.date_to}</Typography>
-          </Box>
-        )
-      },
-      {
-        field: 'total_guest',
-        headerName: 'Total Guest',
-        headerAlign: 'center',
-        align: 'center',
-        width: 150,
-        renderCell: (params) => (
-          <Box>
-            <Typography variant="body2"> {params.row.number_guest}</Typography>
-          </Box>
-        )
-      },
-      {
-        field: 'total_number',
-        headerName: 'Total Number',
-        headerAlign: 'center',
-        align: 'center',
-        width: 150,
-        renderCell: (params) => (
-          <Box>
-            <Typography variant="body2"> {params.row.room}</Typography>
-          </Box>
-        )
-      },
-      {
-        field: 'code',
-        headerName: 'Code',
-        headerAlign: 'center',
-        align: 'center',
-        width: 150,
-        renderCell: (params) => (
-          <Box>
-            <Typography variant="body2"> {params.row.code}</Typography>
-          </Box>
-        )
-      },
-      {
-        field: 'status',
-        headerAlign: 'center',
-        align: 'center',
-        headerName: (
-          <Box sx={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
-            <Typography>Status</Typography>
-          </Box>
-        ),
-        flex: isSmallScreen ? 0 : 1,
-        renderCell: (params) => <TableChip>{params.row.status}</TableChip>
-      },
-
-      {
-        field: 'action',
-        headerName: 'Action',
-        headerAlign: 'center',
-        align: 'center',
-        flex: isSmallScreen ? 0 : 1,
-        renderCell: (params) => (
-          <Button
-            variant="outlined"
-            size="small"
-            onClick={() => handleBookingView(params.row.booking_id)}
-            sx={{ color: '#12A9B2', borderColor: '#12A9B2', borderRadius: 1, '&:focus': { border: 'none' } }}
-          >
-            View
-          </Button>
-        )
-      }
-    ];
-    return columns;
-  };
-
-  const rowsWithCount = lobbyData.map((lobbyTable, index) => ({
-    ...lobbyTable,
-    originalId: index + 1
-  }));
-
-  // const handleBookingView = (id) => {
-  //   setSelectedMeetingId(id);
-  //   setShowMeetingModal(true);
-  // };
-
-  // Usage in your component
-  const adjustedColumns = adjustColumnWidths();
   return (
     <Box>
       <MainCard>
+        <Grid container spacing={2} sx={{ my: 4, display: 'flex', justifyContent: 'end' }}>
+          <Grid item xs={6}></Grid>
+          <Grid item xs={6}>
+            <Grid container spacing={2}>
+              <Grid item xs={7}>
+                <Box>
+                  <DatePicker
+                    onChange={handleChange}
+                    format="YYYY-MM-DD"
+                    style={{ borderRadius: '0px', width: '100%', paddingTop: 8, paddingBottom: 8 }}
+                  />
+                </Box>
+              </Grid>
+              <Grid item xs={5}>
+                <Box sx={{ Width: '100%' }}>
+                  <FormControl fullWidth>
+                    <Select
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      onChange={handleChangeStatus}
+                      sx={{ borderRadius: '0', width: '100%', color: '#8f8f8e' }}
+                      value={handleStatus}
+                      displayEmpty
+                    >
+                      <MenuItem value="" >Selected Status</MenuItem>
+                      <MenuItem value="all">All</MenuItem>
+                      <MenuItem value="pending">Pending</MenuItem>
+                      <MenuItem value="booked">Booked</MenuItem>
+                      <MenuItem value="checkedin">Checked In </MenuItem>
+                      <MenuItem value="checkedout">Checked Out</MenuItem>
+                      <MenuItem value="canceled">Canceled</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Box>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+
         <Box>
           <Typography
             variant="h5"
             color="#fff"
             backgroundColor="#12A9B2"
-            sx={{ px: 3, py: 2, borderRadius: 1, display: 'flex', justifyContent: 'center' }}>
-            Today’s Meeting Schedule
+            sx={{
+              px: 3,
+              py: 2,
+              borderRadius: 1,
+              display: 'flex',
+              justifyContent: 'center',
+              font: 'poppins',
+              fontWeight: 'bold',
+              fontSize: '18px'
+            }}
+          >
+            Booking Schedule
           </Typography>
         </Box>
-        <Box sx={{display:'flex', justifyContent:'end', mt:3}}>
-        <DatePicker/>
-        </Box>
-        {lobbyData.length != 0 ? (
-          <Box>
-            <Box sx={{ display: 'flex', justifyContent: 'end', py: 2 }}>
-              <OutlinedInput
-                id="outlined-adornment-weight"
-                aria-describedby="outlined-weight-helper-text"
-                placeholder="Search"
-                sx={{ border: 1, borderColor: '#12A9B2' }}
-                size="small"
-                endAdornment={
-                  <InputAdornment position="end">
-                    <IconButton>
-                      <Search></Search>
-                    </IconButton>
-                  </InputAdornment>
-                }
-              />
-            </Box>
-            <Box>
-              <Box style={{ width: '95%' }}>
-                <DataGrid
-                  rows={rowsWithCount}
-                  columns={adjustedColumns}
-                  initialState={{
-                    pagination: {
-                      paginationModel: { page: 0, pageSize: 10 }
-                    }
-                  }}
-                  pageSizeOptions={[10, 25, 50, 100]}
-                />
-              </Box>
-            </Box>
-          </Box>
-        ) : (
-          <NoDataImage />
-        )}
 
-        {/* {selectedMeetingId && (
-          <MeetingModal
-            selectedMeetingId={selectedMeetingId}
-            showMeetingModal={showMeetingModal}
-            handleClose={() => setShowMeetingModal(false)}
-          />
-
-          
-        )} */}
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Company Name</TableCell>
+                <TableCell align="right">Phone Number</TableCell>
+                <TableCell align="right">Arrival Time</TableCell>
+                <TableCell align="right">Booking Status</TableCell>
+                <TableCell align="right">Booking For</TableCell>
+                <TableCell align="right">Present</TableCell>
+                <TableCell align="right">QR Code</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {restaurantLists.map((restaurantList) => (
+                <TableRow key={restaurantList.rboksl} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  <TableCell component="th" scope="row">
+                    {restaurantList.company_name}
+                  </TableCell>
+                  <TableCell align="right">{restaurantList.phone_w_codex}</TableCell>
+                  <TableCell align="right">{restaurantList.arrivaltime}</TableCell>
+                  <TableCell align="right">{restaurantList.bkstatus}</TableCell>
+                  <TableCell align="right">{restaurantList.bokfor}</TableCell>
+                  <TableCell align="right">{restaurantList.attncount}</TableCell>
+                  <TableCell align="right">{restaurantList.bkcode}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </MainCard>
     </Box>
   );
